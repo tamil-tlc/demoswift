340ca371d28da06a65e5023b5568f88fe552f3b2
Tamil2031 <tamil21112001>
2025-05-27 18:19:34 +0530

    new file

---
+++ MySwiftPackage/package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "demoswift",
+    targets: [
+        .executableTarget(
+            name: "demoswift",
+            path: "Sources/demoswift"
+        ),
+        .testTarget(
+            name: "demoswiftTests",
+            dependencies: ["demoswift"],
+            path: "Tests/demoswiftTests"
+        )
+    ]
+)
