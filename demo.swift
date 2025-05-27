859f25c3d3b2e61be2a7be7dfa214ad909d82b02
Tamil <127006077+tamil-tlc@users.noreply.github.com>
2025-05-27 16:49:20 +0530

    Update demo.swift
    reduce code line

--- demo.swift
+++ demo.swift
@@ -1,14 +1,2 @@
 import Foundation
-
-// A basic Swift program
-
-// Print a message
 print("Hello, world!")
-
-// Define a function
-func greet(name: String) {
-    print("Hello, \(name)!")
-}
-
-// Call the function
-greet(name: "Alice")
